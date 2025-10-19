8290a1fde56725feb1edf5b78eea75fbd34b9094
lemonnik <lemongamer6484@gmail.com>
2025-10-19 12:37:00 +0200

    init2

---
+++ src/main/java/dev/lemonnik/respinfo/RaspiInfo.java
@@ -0,0 +1,28 @@
+package dev.lemonnik.respinfo;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(RaspiInfo.MODID)
+public class RaspiInfo {
+    public static final String MODID = "raspinfo";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RaspiInfo(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Initializing Raspi Info!");
+    }
+
+}
